3c90661712e4255c114f6d7f512b4819ff3f3a2a
HoangGiaBaoo <hoangbao21022005@gmail.com>
2026-05-13 15:31:40 +0700

    Chức năng đăng nhập

---
+++ QLSV/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace QLSV
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lb_email = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_email = new System.Windows.Forms.TextBox();
+            this.txt_password = new System.Windows.Forms.TextBox();
+            this.btn_dangnhap = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label1.Location = new System.Drawing.Point(173, 77);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(460, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Hệ Thống Quản Lý Sinh Viên";
+            // 
+            // lb_email
+            // 
+            this.lb_email.AutoSize = true;
+            this.lb_email.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_email.Location = new System.Drawing.Point(196, 151);
+            this.lb_email.Name = "lb_email";
+            this.lb_email.Size = new System.Drawing.Size(58, 20);
+            this.lb_email.TabIndex = 1;
+            this.lb_email.Text = "Email:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(196, 191);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Password:";
+            // 
+            // txt_email
+            // 
+            this.txt_email.Location = new System.Drawing.Point(330, 148);
+            this.txt_email.Name = "txt_email";
+            this.txt_email.Size = new System.Drawing.Size(236, 26);
+            this.txt_email.TabIndex = 3;
+            // 
+            // txt_password
+            // 
+            this.txt_password.Location = new System.Drawing.Point(330, 188);
+            this.txt_password.Name = "txt_password";
+            this.txt_password.Size = new System.Drawing.Size(236, 26);
+            this.txt_password.TabIndex = 4;
+            // 
+            // btn_dangnhap
+            // 
+            this.btn_dangnhap.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_dangnhap.Location = new System.Drawing.Point(330, 266);
+            this.btn_dangnhap.Name = "btn_dangnhap";
+            this.btn_dangnhap.Size = new System.Drawing.Size(133, 54);
+            this.btn_dangnhap.TabIndex = 5;
+            this.btn_dangnhap.Text = "Đăng nhập";
+            this.btn_dangnhap.UseVisualStyleBackColor = true;
+            this.btn_dangnhap.Click += new System.EventHandler(this.btn_dangnhap_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_dangnhap);
+            this.Controls.Add(this.txt_password);
+            this.Controls.Add(this.txt_email);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lb_email);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lb_email;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_email;
+        private System.Windows.Forms.TextBox txt_password;
+        private System.Windows.Forms.Button btn_dangnhap;
+    }
+}
+
---
+++ QLSV/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QLSV
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_dangnhap_Click(object sender, EventArgs e)
+        {
+            string email = txt_email.Text;
+            string password = txt_password.Text;
+            if(email == "0002568@st.huce.edu.vn" &&  password == "0002568")
+            {
+                MessageBox.Show("Đăng nhập thành công!");
+            }
+            else
+            {
+                MessageBox.Show("Đăng nhập thất bại!");
+            }
+        }
+    }
+}
